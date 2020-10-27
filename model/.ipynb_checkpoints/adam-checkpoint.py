cb6b564311a638dc31927d015f675dad90f1bb22
HaoranZhuExplorer <hz1922@nyu.edu>
2020-10-26 23:02:18 -0400

    fix ewc bug

---
+++ model/.ipynb_checkpoints/adam-checkpoint.py
@@ -0,0 +1,102 @@
+### This is a copy of GEM from https://github.com/facebookresearch/GradientEpisodicMemory.
+### In order to ensure complete reproducability, we do not change the file and treat it as a baseline.
+
+# Copyright 2019-present, IBM Research
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+
+# import numpy as np
+# import quadprog
+
+from .common import MLP, ResNet18, vgg11_bn
+
+
+# Auxiliary functions useful for GEM's inner optimization.
+
+def compute_offsets(task, nc_per_task, is_cifar):
+    """
+        Compute offsets for cifar to determine which
+        outputs to select for a given task.
+    """
+    if is_cifar:
+        offset1 = task * nc_per_task
+        offset2 = (task + 1) * nc_per_task
+    else:
+        offset1 = 0
+        offset2 = nc_per_task
+    return offset1, offset2
+
+class Net(nn.Module):
+    def __init__(self,
+                 n_inputs,
+                 n_outputs,
+                 n_tasks,
+                 args):
+        super(Net, self).__init__()
+        nl, nh = args.n_layers, args.n_hiddens
+        self.margin = args.memory_strength
+        self.is_cifar = (args.data_file == 'cifar100.pt' or
+                         args.data_file == 'cifar100_20.pt' or
+                         args.data_file == 'cifar100_20_o.pt')
+        if self.is_cifar:
+            self.net = vgg11_bn()
+            # self.net = ResNet18(n_outputs)
+        else:
+            self.net = MLP([n_inputs] + [nh] * nl + [n_outputs])
+
+        self.ce = nn.CrossEntropyLoss()
+        self.n_outputs = n_outputs
+
+        self.opt = optim.Adam(self.parameters(), args.lr)
+
+        self.n_memories = args.n_memories
+        self.n_tasks = n_tasks
+        self.gpu = args.cuda
+
+        # allocate counters
+        self.observed_tasks = []
+        self.old_task = -1
+        self.mem_cnt = 0
+        if self.is_cifar:
+            self.nc_per_task = int(n_outputs / n_tasks)
+        else:
+            self.nc_per_task = n_outputs
+
+        if args.cuda:
+            self.cuda()
+
+    def forward(self, x, t):
+        output = self.net(x)
+        if self.is_cifar:
+            # make sure we predict classes within the current task
+            offset1 = int(t * self.nc_per_task)
+            offset2 = int((t + 1) * self.nc_per_task)
+            if offset1 > 0:
+                output[:, :offset1].data.fill_(-10e10)
+            if offset2 < self.n_outputs:
+                output[:, offset2:self.n_outputs].data.fill_(-10e10)
+        return output
+
+    def update(self, x, t, y):
+        # update memory
+        if t != self.old_task:
+            self.observed_tasks.append(t)
+            self.old_task = t
+
+        # now compute the grad on the current minibatch
+        self.zero_grad()
+
+        offset1, offset2 = compute_offsets(t, self.nc_per_task, self.is_cifar)
+        loss = self.ce(self.forward(x, t)[:, offset1: offset2], y - offset1)
+        loss.backward()
+
+        self.opt.step()
+
+        return loss
